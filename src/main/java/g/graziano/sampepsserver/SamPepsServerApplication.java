56c44144885d0d864b3bf952f6da92e8b0cadb14
ggraziano <g.graziano94@gmail.com>
2018-05-31 14:16:44 +0200

    newwww

---
+++ src/main/java/g/graziano/sampepsserver/SamPepsServerApplication.java
@@ -0,0 +1,12 @@
+package g.graziano.sampepsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamPepsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamPepsServerApplication.class, args);
+	}
+}
