01335047e97c87f76542d2339e786da9da8dab7b
Giuseppe Graziano <g.graziano1@icloud.com>
2018-05-31 20:39:43 +0200

    fam

--- src/main/java/g/graziano/sampepsserver/configuration/SecurityConfiguration.java
+++ src/main/java/g/graziano/sampepsserver/configuration/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public UserDetailsService userDetailsService() {
         UserDetails user =
-                User.builder().e.username("user")
+                User.builder().username("user")
                         .password("password")
                         .roles("USER")
                         .build();
