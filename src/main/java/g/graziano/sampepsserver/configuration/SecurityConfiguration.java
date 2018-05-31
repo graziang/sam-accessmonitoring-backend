a363e3defaa6c7fae3ae12569fd154ee04540051
ggraziano <g.graziano94@gmail.com>
2018-05-31 15:53:30 +0200

    asdf

--- src/main/java/g/graziano/sampepsserver/configuration/SecurityConfiguration.java
+++ src/main/java/g/graziano/sampepsserver/configuration/SecurityConfiguration.java
@@ -16,7 +16,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/test").permitAll()
+                .antMatchers("test").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
