ac70b54cf72b71c71a7a3e0992b7b564bfbc28a6
Giuseppe Graziano <g.graziano1@icloud.com>
2018-06-18 23:52:14 +0200

    android notification

---
+++ src/main/java/g/graziano/sampepsserver/HeaderRequestInterceptor.java
@@ -0,0 +1,4 @@
+package g.graziano.sampepsserver;
+
+public class HeaderRequestInterceptor {
+}
---
+++ src/main/java/g/graziano/sampepsserver/service/AndroidNotificationsService.java
@@ -0,0 +1,4 @@
+package g.graziano.sampepsserver.service;
+
+public class AndroidNotificationsService {
+}
